eb59388391a2429725566254f7c93874f876860b
souky <136069803@qq.com>
2019-12-19 11:17:51 +0800

    初次提交

---
+++ src/plugins/element.js
@@ -0,0 +1,36 @@
+import Vue from 'vue'
+import {
+  Button,
+  Input,
+  Radio,
+  RadioGroup,
+  RadioButton,
+  Checkbox,
+  CheckboxButton,
+  CheckboxGroup,
+  Switch,
+  Select,
+  Option,
+  OptionGroup,
+  ButtonGroup,
+  DatePicker,
+  Form,
+  FormItem,
+} from 'element-ui'
+
+Vue.use(Button);
+Vue.use(Input);
+Vue.use(Radio);
+Vue.use(RadioGroup);
+Vue.use(RadioButton);
+Vue.use(Checkbox);
+Vue.use(CheckboxButton);
+Vue.use(CheckboxGroup);
+Vue.use(Switch);
+Vue.use(Select);
+Vue.use(Option);
+Vue.use(OptionGroup);
+Vue.use(ButtonGroup);
+Vue.use(DatePicker);
+Vue.use(Form);
+Vue.use(FormItem);
